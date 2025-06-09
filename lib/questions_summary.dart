6aba3e684b423a303409f0a9a70417b8a02c0615
Scot Pfuntner <spinatose@gmail.com>
2025-06-09 06:57:36 -0500

    more formatting on questions summary widget

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -2,49 +2,63 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsSummary extends StatelessWidget {
-  const QuestionsSummary({
-    super.key,
-    required this.summaryData,
-  });
+  const QuestionsSummary({super.key, required this.summaryData});
 
   final List<Map<String, Object>> summaryData;
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: summaryData.map(
-        (data) {
-          // final question = data['question'] as QuizQuestion;
-          // final selectedAnswer = data['selectedAnswer'] as String;
-          // final correctAnswer = data['correctAnswer'] as String;
-          final isCorrect = data['isCorrect'] as bool;
+      children:
+          summaryData.map((data) {
+            final isCorrect = data['isCorrect'] as bool;
 
-          return Row(
-            children: [
-              Text(((data['questionIndex'] as int) + 1).toString()),
-              Column(children: [
-                Text(data['question'] as String,
-                    style: GoogleFonts.aBeeZee(
-                      color: const Color.fromARGB(255, 233, 147, 187),
-                      fontSize: 16,
-                    )),
-                SizedBox(height: 5),
-                Text(data['selectedAnswer'] as String,
-                    style: GoogleFonts.aBeeZee(
-                      color: isCorrect
-                          ? const Color.fromARGB(255, 17, 147, 223)
-                          : Colors.red,
-                      fontSize: 14,
-                    )),
-                Text(data['correctAnswer'] as String,
-                    style: GoogleFonts.aBeeZee(
-                      color:const Color.fromARGB(255, 17, 147, 223),
-                      fontSize: 14,
-                    )),
-              ],)
-          ]);
-        },
-      ).toList(),
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    ((data['questionIndex'] as int) + 1).toString(),
+                    style: GoogleFonts.aBeeZee(fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(width: 12),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          data['question'] as String,
+                          style: GoogleFonts.aBeeZee(
+                            color: const Color.fromARGB(255, 233, 147, 187),
+                            fontSize: 16,
+                          ),
+                        ),
+                        const SizedBox(height: 5),
+                        Text(
+                          data['selectedAnswer'] as String,
+                          style: GoogleFonts.aBeeZee(
+                            color:
+                                isCorrect
+                                    ? const Color.fromARGB(255, 17, 147, 223)
+                                    : Colors.red,
+                            fontSize: 14,
+                          ),
+                        ),
+                        Text(
+                          data['correctAnswer'] as String,
+                          style: GoogleFonts.aBeeZee(
+                            color: const Color.fromARGB(255, 17, 147, 223),
+                            fontSize: 14,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            );
+          }).toList(),
     );
   }
   // Widget build(BuildContext context) {
@@ -77,4 +91,4 @@ class QuestionsSummary extends StatelessWidget {
   //     ),
   //   );
   // }
-} 
+}
