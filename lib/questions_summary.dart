23cade6ee9e87f1380acc3e7ff88d87ab4a5ce12
Scot Pfuntner <spinatose@gmail.com>
2025-06-10 07:05:30 -0500

    fixed format of summary results on results screen

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -8,87 +8,115 @@ class QuestionsSummary extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children:
-          summaryData.map((data) {
-            final isCorrect = data['isCorrect'] as bool;
+    return Expanded(
+      child: ListView.builder(
+        itemCount: summaryData.length,
+        itemBuilder: (context, index) {
+          final question = summaryData[index]['question'] as String;
+          final selectedAnswer = summaryData[index]['selectedAnswer'] as String;
+          final correctAnswer = summaryData[index]['correctAnswer'] as String;
+          final isCorrect = summaryData[index]['isCorrect'] as bool;
 
-            return Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8.0),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    ((data['questionIndex'] as int) + 1).toString(),
-                    style: GoogleFonts.aBeeZee(fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(width: 12),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          data['question'] as String,
-                          style: GoogleFonts.aBeeZee(
-                            color: const Color.fromARGB(255, 233, 147, 187),
-                            fontSize: 16,
-                          ),
-                        ),
-                        const SizedBox(height: 5),
-                        Text(
-                          data['selectedAnswer'] as String,
-                          style: GoogleFonts.aBeeZee(
-                            color:
-                                isCorrect
-                                    ? const Color.fromARGB(255, 17, 147, 223)
-                                    : Colors.red,
-                            fontSize: 14,
-                          ),
-                        ),
-                        Text(
-                          data['correctAnswer'] as String,
-                          style: GoogleFonts.aBeeZee(
-                            color: const Color.fromARGB(255, 17, 147, 223),
-                            fontSize: 14,
-                          ),
-                        ),
-                      ],
+          return ListTile(
+            title: Row(
+              children: [
+                Text(
+                  '${index + 1}. ',
+                  style: GoogleFonts.aBeeZee(
+                    color: const Color.fromARGB(255, 17, 147, 223),
+                    fontWeight: FontWeight.bold,
+                  ), 
+                ),
+                Expanded(
+                  child: Text(
+                    question,
+                    style: GoogleFonts.aBeeZee(
+                      color: const Color.fromARGB(255, 233, 147, 187),
+                      fontSize: 16,
                     ),
                   ),
-                ],
-              ),
-            );
-          }).toList(),
+                ),
+              ],
+            ),
+            subtitle: Row(
+              children: [
+                SizedBox(width: 20),
+                Expanded(
+                  child: Text(
+                    isCorrect
+                        ? 'Your correct answer: $correctAnswer'
+                        : 'Your answer: $selectedAnswer\nCorrect answer: $correctAnswer',
+                    style: GoogleFonts.aBeeZee(
+                      color:
+                          isCorrect
+                              ? const Color.fromARGB(255, 17, 147, 223)
+                              : Colors.red,
+                      fontSize: 14,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
-  // Widget build(BuildContext context) {
-  //   return Expanded(
-  //     child: ListView.builder(
-  //       itemCount: summaryData.length,
-  //       itemBuilder: (context, index) {
-  //         final question = summaryData[index]['question'] as QuizQuestion;
-  //         final selectedAnswer = summaryData[index]['selectedAnswer'] as String;
-  //         final correctAnswer = summaryData[index]['correctAnswer'] as String;
-  //         final isCorrect = summaryData[index]['isCorrect'] as bool;
 
-  //         return ListTile(
-  //           title: Text(
-  //             question.text,
-  //             style: GoogleFonts.aBeeZee(
-  //               color: const Color.fromARGB(255, 233, 147, 187),
-  //               fontSize: 16,
+  // Widget build(BuildContext context) {
+  //   return Column(
+  //     children:
+  //         summaryData.map((data) {
+  //           final isCorrect = data['isCorrect'] as bool;
+
+  //           return Expanded(
+  //             child: Padding(
+  //               padding: const EdgeInsets.symmetric(vertical: 8.0),
+  //               child: Row(
+  //                 crossAxisAlignment: CrossAxisAlignment.start,
+  //                 children: [
+  //                   Text(
+  //                     ((data['questionIndex'] as int) + 1).toString(),
+  //                     style: GoogleFonts.aBeeZee(fontWeight: FontWeight.bold),
+  //                   ),
+  //                   const SizedBox(width: 12),
+  //                   Expanded(
+  //                     child: Column(
+  //                       crossAxisAlignment: CrossAxisAlignment.start,
+  //                       children: [
+  //                         Text(
+  //                           data['question'] as String,
+  //                           style: GoogleFonts.aBeeZee(
+  //                             color: const Color.fromARGB(255, 233, 147, 187),
+  //                             fontSize: 16,
+  //                           ),
+  //                         ),
+  //                         const SizedBox(height: 5),
+  //                         Text(
+  //                           data['selectedAnswer'] as String,
+  //                           style: GoogleFonts.aBeeZee(
+  //                             color:
+  //                                 isCorrect
+  //                                     ? const Color.fromARGB(255, 17, 147, 223)
+  //                                     : Colors.red,
+  //                             fontSize: 14,
+  //                           ),
+  //                         ),
+  //                         Text(
+  //                           data['correctAnswer'] as String,
+  //                           style: GoogleFonts.aBeeZee(
+  //                             color: const Color.fromARGB(255, 17, 147, 223),
+  //                             fontSize: 14,
+  //                           ),
+  //                         ),
+  //                       ],
+  //                     ),
+  //                   ),
+  //                 ],
+  //               ),
   //             ),
-  //           ),
-  //           subtitle: Text(
-  //             isCorrect ? 'Your correct answer: $correctAnswer' : 'Your answer: $selectedAnswer\nCorrect answer: $correctAnswer',
-  //             style: GoogleFonts.aBeeZee(
-  //               color: isCorrect ? const Color.fromARGB(255, 17, 147, 223) : Colors.red,
-  //               fontSize: 14,
-  //             ),
-  //           ),
-  //         );
-  //       },
-  //     ),
+  //           );
+  //         }).toList(),
   //   );
   // }
 }
